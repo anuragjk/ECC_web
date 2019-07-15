8f80370c5b8b6a75a2fe5d35c5240d2331a73d34
Anurag Jk <anurag.zxcv@gmail.com>
2019-07-15 16:10:19 +0530

    In working condition

--- webApp.py
+++ webApp.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+import datetime
 app = Flask(__name__)
-@app.route('/')
-def index():
-    return 'Hello world'
-if __name__ == '__main__':
-    app.run(debug=True, port=80, host='0.0.0.0')
+@app.route("/")
+def hello():
+   now = datetime.datetime.now()
+   timeString = now.strftime("%Y-%m-%d %H:%M")
+   templateData = {
+      'Company_name' : 'HELLO!',
+      }
+   return render_template('index.html', **templateData)
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=80, debug=True)
